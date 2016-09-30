a70b50674cc7539411a95257cea163c8ca34fd8d
Xavier Francisco <xavier.n.francisco@gmail.com>
2016-09-30 03:06:46 +0100

    up

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 import subprocess
+from os.path import isfile
+import os.path
+from os import listdir
 from flask import Flask, render_template
 app = Flask(__name__)
+
+dir = os.path.expanduser('~/Queue/')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        subprocess.run(['juke', 'add', request.form['song'])
+        attrib = "youtube={}".format(request.form['song'])
+        
+        with open(request.form['song']) as f:
+            f.write(attrib)
 
-    return render_template('home.html') # add list of musics to template
+    return render_template('home.html') # TODO: add list of musics to template
