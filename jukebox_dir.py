dac9ea1d5a4de8973929abc1152c2309bd367181
Xavier Francisco <xavier.n.francisco@gmail.com>
2016-09-30 03:36:07 +0100

    sort by creation date

--- jukebox_dir.py
+++ jukebox_dir.py
@@ -5,23 +5,17 @@ import configparser
 import itertools
 import shutil
 
-logger = logging.getLogger(__name__)
-level = logging.DEBUG
-# level = logging.INFO
-try:
-    import coloredlogs
-    coloredlogs.install(level=level)
-except ImportError:
-    logging.basicConfig(level=level)
-    logger.info("Not using coloredlogs, because coloredlogs is not installed.")
+mpv = lambda file: os.system("/usr/bin/mpv {} --no-video".format(file))
+mpg123 = lambda file: os.system("/usr/bin/mpg123 {}".format(file))
 
-try:
-    CACHE = get_cache()
-except:
-    e = sys.exc_info()[0]
-    logger.warning(e)
-    CACHE = None
+def music_queue(path):
+    while 1:
+        files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
+        if files:
+            yield min(files, key=lambda f: os.path.getctime(f))
+        else:
+            break
 
 def read_properties_file(filepath):
     """
@@ -34,64 +28,11 @@ def read_properties_file(filepath):
 
     return dict(parser.items('top'))
 
-# def get_cache():
-#     home_cache = os.environ['XDG_CACHE_HOME']
-#     cache = os.path.join(home_cache, 'jukebox')
-
-#     if os.isdir(cache):
-#         logger.debug("Cache exists.")
-#     elif os.i.sfile(cache):
-#         logger.warning("Cache is a file! Remove this file to enable cache.")
-#         return None
-#     else: # doesn't exist
-#         logger.debug("Cache doesn't exists. Creating it...")
-#         os.mkdir(cache)
-#         logger.debug("Cache created.")
-
-#     return cache
-
-def next_music(files):
-
-    def sortkey(pathfile):
-        n = ""
-        for char in os.path.basename(pathfile):
-            if not(char.isdigit()):
-                break
-            n += char
-
-        return int(n)
-
-    # logger.debug("Excluding these files: {}".format(
-    #     [f for f in files if not(os.path.basename(f)[0].isdigit())]))
-    
-    return min([f for f in files if os.path.basename(f)[0].isdigit()], key=sortkey)
-
-
-def play_dir(path):
-    while 1:
-        files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-        logger.debug("Found {} files in path".format(len(files)))
-        
-        try:
-            next = next_music(files)
-        except ValueError:
-            logger.debug("No musics on queue. Exiting")
-            break
-
-        logger.debug("Playing from file {}".format(next))
-        play_file(next)
-        logger.debug("Removed {}".format(next))
-        os.remove(next)
-
-mpv = lambda file: os.system("/usr/bin/mpv {} --no-video".format(file))
-mpg123 = lambda file: os.system("/usr/bin/mpg123 {}".format(file))
-
 def play_file(path):
-
     props = read_properties_file(path)
 
-    if 'cache' in props:
-        f = props['cache']
+    if 'mp3' in props:
+        f = props['mp3']
         mpg123(f)
 
     elif 'file' in props:
@@ -102,7 +43,10 @@ def play_file(path):
         yt_link = props['youtube']
         mpv(yt_link)
 
-
+def play_dir(path):
+    for music in music_queue(path):
+        play_file(music)
+        os.remove(music)
 
 def main():
     play_dir(sys.argv[1])
